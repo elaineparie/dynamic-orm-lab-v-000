125a373cf253af15d9c5ff669322706d45130a89
Elaine Anderson <eparie@bu.edu>
2018-07-03 19:22:18 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -54,6 +54,6 @@ def self.find_by_name(name)
   end
 
 def self.find_by(attribute)
-  sql = "SELECT * FROM #{self.table_name} WHERE #{column_names} = attribute"
+  sql = "SELECT * FROM #{self.table_name} WHERE #{column_names} = 'attribute'"
 end
 end
